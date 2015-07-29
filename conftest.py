dcae81a105340b1e513fc17ad52264bb41dccc09
Alex360-D <alex360.dors@yandex.ru>
2015-07-29 22:11:54 +0300

    Задание №24. Параметризация тестов данными из Excel
    Лучше поздно, чем никогда!!!

--- conftest.py
+++ conftest.py
@@ -1,7 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from model.group import Group
 import pytest
+import os.path
 from fixture.application import Application2
+
+import clr
+clr.AddReferenceByName('Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
+from Microsoft.Office.Interop import Excel
 
 fixture = None
 
@@ -18,3 +24,22 @@ def stop(request):
         fixture.close_application()
     request.addfinalizer(fin)
     return fixture
+
+def pytest_generate_tests(metafunc):
+    for fixture in metafunc.fixturenames:
+        if fixture.startswith("xlsx_"):
+            testdata = load_from_xlsx(fixture[5:])
+            metafunc.parametrize(fixture, testdata, ids=[str(x) for x in testdata])
+
+def load_from_xlsx(file):
+    f = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data\%s.xlsx" % file)
+    excel = Excel.ApplicationClass()
+    excel.Visible = False
+    workbook = excel.Workbooks.Open(r"%s" % f)
+    sheet = workbook.Worksheets[1]
+    testdata = []
+    for i in range(1,7):
+        text = sheet.Rows[i].Value2[0,0]
+        testdata.append(Group(name = text))
+    excel.Quit()
+    return testdata
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,17 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from model.group import Group
-import random
-import string
-
-def test_add_group(app):
+def test_add_group(app, xlsx_groups):
+    group = xlsx_groups
     old_list = app.group.get_group_list(app.main_window)
-    group = Group(name=random_string("group_", 10))
     app.group.add_new_group(app.main_window, group)
     new_list = app.group.get_group_list(app.main_window)
     old_list.append(group.name)
     assert sorted(old_list) == sorted(new_list)
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*5
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
